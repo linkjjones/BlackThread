9510503947fae5ca3e6373392615942699905212
jones <linkjjones@gmail.com>
2020-09-23 13:04:55 -0400

    Use model in category view

---
+++ JamiiWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JamiiWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JamiiWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // crate some dummy data
+            var categories = new List<Category>();
+            // make 10 fake objects
+            for (int i = 0; i < 10; i++)
+            {
+                categories.Add(new Category() { Id = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass the populated list to the view to display
+            return View(categories);
+        }
+
+        public IActionResult Browse(string category)
+        {
+            // pass incoming category to the Browse view using the ViewBag object
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
